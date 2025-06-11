76e2291549c5728dcb1f31a4f3f3197131cdb01f
TheBloodLess <joshuaburgos1101@gmail.com>
2025-06-11 13:58:52 -0500

    hola

---
+++ clases/src/main/java/com/example/Administrador.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class Administrador extends Usuario{
+    
+    public Usuario crearUsuarioRol(String usuario, String password){
+        throw new UnsupportedOperationException();
+    }
+
+    public void asignarPermiso(String Usuario){
+        throw new UnsupportedOperationException();
+    }
+
+    public void actualizarSoftware(){
+        throw new UnsupportedOperationException();
+    }
+
+    public void asignarResponsable(){
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ clases/src/main/java/com/example/Curso.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Curso {
+    private String nombre;
+    private String id;
+    
+}
